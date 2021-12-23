e633af5e193c27005da3a329c03376b12c5ad99f
Adam Rusi <95165471+FoolishlyAccused@users.noreply.github.com>
2021-12-23 19:15:00 +0100

    Update Player Reset & Wait Until Player Changes In Between Script.lua

--- Player Reset & Wait Until Player Changes In Between Script.lua
+++ Player Reset & Wait Until Player Changes In Between Script.lua
@@ -1,5 +1,8 @@
-game.Players.LocalPlayer.Character.Head:Destroy()
+
+
+game.Players.LocalPlayer.Character.Head:Destroy() --resets player
 wait(8)
+-----------------------------------if the player dies it will reset the script
 game:GetService("RunService").Stepped:connect(function()
 game.Players.LocalPlayer.Character:WaitForChild("Humanoid"):ChangeState(11)
 end) end)
@@ -8,3 +11,5 @@ Clone = game:GetService("Players").LocalPlayer.Character.LowerTorso.Root:Clone()
   game:GetService("Players").LocalPlayer.Character.LowerTorso.Root:Destroy()
   Clone.Parent = game:GetService("Players").LocalPlayer.Character
 end)
+-------------------------------
+--script of your chouice goes after this
